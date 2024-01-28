9ab3eb024ff8973e5ecf15b4ef7165c2b6424a8b
ashmawymidxd <150594610+ashmawymidxd@users.noreply.github.com>
2024-01-28 18:36:50 +0200

    Delete resources directory
    d

--- resources/views/Dashboard/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer opened -->
-<div class="">
-    <div class="container-fluid d-flex align-items-center justify-content-center">
-      <div class="row col-md-12  p-3 card text-center" style="background-color: white;">
-        all right &copy; are saved
-      </div>
-    </div>
-  </div>
-<!-- Footer closed -->
